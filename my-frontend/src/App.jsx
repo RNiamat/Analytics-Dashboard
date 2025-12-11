397698380c393a704db6d4ee76e0274b8a57e896
Rafia Neyamat <rnvisualdesignsco@gmail.com>
2025-12-11 23:34:59 +0500

    Setup of frontend in react/vite

---
+++ my-frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from 'react'
+function App() {
+  return <h1>Hello from React</h1>;
+}
+
+export default App
